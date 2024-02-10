93e2efc63ef645acd35502afb923615f382178d0
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-10 01:57:14 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <Text>Kayo Ennrique</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
