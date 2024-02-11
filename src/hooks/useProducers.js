af972db22dfde3979b68fad22f3cd7f10a2c71ca
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-11 01:06:57 -0300

    feat(git): ordering producers

--- src/hooks/useProducers.js
+++ src/hooks/useProducers.js
@@ -7,6 +7,9 @@ export default function useProducers() {
 
   useEffect(() => {
     const retorno = loadingProducers();
+    retorno.list.sort(
+      (productor1, productor2) => productor1.distance - productor2.distance,
+    );
     setTitle(retorno.title);
     setList(retorno.list);
   }, []);
